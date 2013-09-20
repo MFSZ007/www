bd6fdc2cc479f5957984adb41a68ddf90958f356
micsur <mz@marcelozaifrani.com.ar>
2013-09-20 16:38:14 -0300

    Create act_constantes.php

---
+++ act_constantes.php
@@ -0,0 +1,8 @@
+<?PHP	$color_fondo		= "ff0000";
+$color_fondo_body	= "ffffff";
+$color_fondo_home	= "8D0F0F";
+$color_fondo_home2	= "4275BA";
+$color_fondo_oscuro	= "3F3F3F";
+$color_fondo_claro	= "F0EEEE";
+$color_fondo_claro2	= "D3D3D3";
+$color_fondo_color	= "0000ff";	?>
